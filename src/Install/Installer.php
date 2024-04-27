3a879a162f501fb4d6a3f4e6669eeccf9b0b03b8
Dylan Ramos <dylan.ramos.n@gmail.com>
2024-04-27 02:11:10 +0200

    Add installation files for ReCaptcha module
    The commit includes the creation of new installation files for the ReCaptcha module, such as InstallerFactory and Installer classes. These files are used to handle the module's installation and uninstallation process. In addition, several .htaccess files were added to limit direct access to specified directories.

---
+++ src/Install/Installer.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DrSoftFr\Module\ReCaptcha\Install;
+
+use DrSoftFr\Module\ReCaptcha\Data\Configuration\ReCaptchaConfiguration;
+use Module;
+use Throwable;
+
+/**
+ * Class responsible for modifications needed during installation/uninstallation of the module.
+ */
+final class Installer
+{
+    const HOOKS = [];
+
+    /**
+     * @var ReCaptchaConfiguration
+     */
+    private $recaptchaConfiguration;
+
+    /**
+     * @param ReCaptchaConfiguration $recaptchaConfiguration
+     */
+    public function __construct(
+        ReCaptchaConfiguration $recaptchaConfiguration
+    )
+    {
+        $this->recaptchaConfiguration = $recaptchaConfiguration;
+    }
+
+    /**
+     * Module's installation entry point.
+     *
+     * @param Module $module
+     *
+     * @return bool
+     */
+    public function install(Module $module): bool
+    {
+        try {
+            if (!$this->registerHooks($module)) {
+                return false;
+            }
+
+            $this->recaptchaConfiguration->initConfiguration();
+        } catch (Throwable $t) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Module's uninstallation entry point.
+     *
+     * @param Module $module
+     *
+     * @return bool
+     */
+    public function uninstall(Module $module): bool
+    {
+        try {
+            $this->recaptchaConfiguration->removeConfiguration();
+        } catch (Throwable $t) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Register hooks for the module.
+     *
+     * @param Module $module
+     *
+     * @return bool
+     */
+    private function registerHooks(Module $module): bool
+    {
+        return (bool)$module->registerHook(self::HOOKS);
+    }
+
+    /**
+     * @return ReCaptchaConfiguration
+     */
+    public function getReCaptchaConfiguration(): ReCaptchaConfiguration
+    {
+        return $this->recaptchaConfiguration;
+    }
+}
