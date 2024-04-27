d9769488d60ddf21ceba37bfbe27ebedd0610ba0
Dylan Ramos <dylan.ramos.n@gmail.com>
2024-04-27 14:47:42 +0200

    Add reset and save features to ReCaptcha module
    Added a path to reset and save ReCaptcha configuration settings in the routes file and corresponding methods in the controller file. Ensure better error handling and user feedback on successful operations or in event of an exception.

--- src/Controller/Admin/ReCaptchaController.php
+++ src/Controller/Admin/ReCaptchaController.php
@@ -4,13 +4,16 @@ declare(strict_types=1);
 
 namespace DrSoftFr\Module\ReCaptcha\Controller\Admin;
 
+use DrSoftFr\Module\ReCaptcha\Data\Configuration\ReCaptchaConfiguration;
 use drsoftfrrecaptcha;
 use PrestaShop\PrestaShop\Core\Form\FormHandlerInterface;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShopBundle\Security\Annotation\AdminSecurity;
 use PrestaShopBundle\Security\Annotation\ModuleActivated;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 /**
  * Class ReCaptchaController.
@@ -37,7 +40,7 @@ final class ReCaptchaController extends FrameworkBundleAdminController
     public function indexAction(Request $request): Response
     {
         $form = $this
-            ->getSettingFormHandler()
+            ->getReCaptchaFormHandler()
             ->getForm();
 
         return $this->render('@Modules/drsoftfrrecaptcha/views/templates/admin/index.html.twig', [
@@ -49,6 +52,116 @@ final class ReCaptchaController extends FrameworkBundleAdminController
     }
 
     /**
+     * Reset setting
+     *
+     * @AdminSecurity(
+     *     "is_granted('update', request.get('_legacy_controller'))",
+     *     redirectRoute="admin_drsoft_fr_recaptcha_index",
+     *     message="You do not have permission to reset this."
+     * )
+     *
+     * @return RedirectResponse
+     */
+    public function resetAction(): RedirectResponse
+    {
+        try {
+            $this
+                ->getReCaptchaConfiguration()
+                ->initConfiguration();
+
+            $this->addFlash(
+                'success',
+                $this->trans(
+                    'The default setting for ReCAPTCHA are reset.',
+                    'Modules.Drsoftfrrecaptcha.Success'
+                )
+            );
+        } catch (Throwable $t) {
+            $this->addFlash(
+                'error',
+                $this->trans(
+                    'Cannot reset the setting. Throwable: #%code% - %message%',
+                    'Modules.Drsoftfrrecaptcha.Error',
+                    [
+                        '%code%' => $t->getCode(),
+                        '%message%' => $t->getMessage(),
+                    ]
+                )
+            );
+        }
+
+        return $this->redirectToRoute('admin_drsoft_fr_recaptcha_index');
+    }
+
+    /**
+     * Edit setting
+     *
+     * @AdminSecurity(
+     *     "is_granted('update', request.get('_legacy_controller'))",
+     *     redirectRoute="admin_drsoft_fr_recaptcha_index",
+     *     message="You do not have permission to edit this."
+     * )
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function saveAction(Request $request): Response
+    {
+        try {
+            $handler = $this->getReCaptchaFormHandler();
+
+            $form = $handler->getForm();
+            $form->handleRequest($request);
+
+            if (!$form->isSubmitted()) {
+                return $this->redirectToRoute('admin_drsoft_fr_recaptcha_index');
+            }
+
+            if (!$form->isValid()) {
+                $this->addFlash(
+                    'error',
+                    $this->trans(
+                        'The form is invalid.',
+                        'Modules.Drsoftfrrecaptcha.Error'
+                    )
+                );
+
+                return $this->redirectToRoute('admin_drsoft_fr_recaptcha_index');
+            }
+
+            $errors = $handler->save($form->getData());
+
+            if (!empty($errors)) {
+                $this->flashErrors($errors);
+            } else {
+                $this->addFlash(
+                    'success',
+                    $this->trans(
+                        'Your setting for ReCAPTCHA are saved.',
+                        'Modules.Drsoftfrrecaptcha.Success'
+                    )
+                );
+            }
+
+        } catch (Throwable $t) {
+            $this->addFlash(
+                'error',
+                $this->trans(
+                    'Cannot save the setting. Throwable: #%code% - %message%',
+                    'Modules.Drsoftfrrecaptcha.Error',
+                    [
+                        '%code%' => $t->getCode(),
+                        '%message%' => $t->getMessage(),
+                    ]
+                )
+            );
+        }
+
+        return $this->redirectToRoute('admin_drsoft_fr_recaptcha_index');
+    }
+
+    /**
      * @return drsoftfrrecaptcha|object
      */
     protected function getModule()
@@ -57,11 +170,21 @@ final class ReCaptchaController extends FrameworkBundleAdminController
     }
 
     /**
-     * Get setting form handler.
+     * Get ReCAPTCHA configuration.
+     *
+     * @return ReCaptchaConfiguration
+     */
+    protected function getReCaptchaConfiguration(): ReCaptchaConfiguration
+    {
+        return $this->get('drsoft_fr.module.recaptcha.data.configuration.recaptcha_configuration');
+    }
+
+    /**
+     * Get ReCAPTCHA form handler.
      *
      * @return FormHandlerInterface
      */
-    protected function getSettingFormHandler(): FormHandlerInterface
+    protected function getReCaptchaFormHandler(): FormHandlerInterface
     {
         return $this->get('drsoft_fr.module.recaptcha.form.handler.recaptcha_form_handler');
     }
